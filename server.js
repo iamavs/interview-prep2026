45efb27bc0befc92e94b88873c88683e8b95e78a
root <root@ip-172-31-20-46.ap-southeast-2.compute.internal>
2026-05-23 03:12:56 +0000

    Updated Server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Assessment App Running!:v2");
+  res.send("DevOps Assessment App Running!:v22");
 });
 
 app.listen(3000, () => {
